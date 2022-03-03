9463714b4401ea8638dcebddab86c8c9c2fdae01
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-03-02 20:40:20 -0500

    changes

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,6 +1,7 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
 height = window.innerHeight * 0.7;
+margin - 50;
 
 /* LOAD DATA */
 d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
@@ -8,19 +9,35 @@ d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
     console.log(data)
 
     /* SCALES */
-    cons xScale = d3.scaleLinear ()
+    const xScale = d3.scaleLinear ()
     .domain([0,d3.max(data, d => d.envScore2020)])
     .range([0,width-margin])
 
     const yScale = d3.scaleLinear()
     .domain([0,d3.max(data, d => d.ideologyScore2020)])
-    .range(height-margin, 0)
+    .range[([height-margin, margin])
+
+    const colorScale = d3.scaleOrdinal()
+    .domain(["R", "D", "I"])
+    .range(["red", "blue", "purple"])
     
     /* HTML ELEMENTS */
     const svg = d3.select("#container")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale)
+
+    svg.append("g")
+    .attr("transform", 'translate(0,${height-margin})')
+    .call(xAxis)
+
+    svg.append("g")
+    .attr("transform","translate(0,0")
+    .call(yAxis)
+
 
     /* JOIN - SELECT-DATA-JOIN & DRAW */
     svg.selectAll("circle")
@@ -29,6 +46,6 @@ d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
     .attr("cx", d => xScale(d.envScore2020))
     .attr("cy", d => yScale(d.ideologyScore2020))
     .attr("r", 3)
-
+    .attr("fill", d=> colorScale(d.Party))
 
   });
--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,21 +1,34 @@
  /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+const width = window.innerWidght * .7,
+height = window.innerHeight * .7,
+margih = 30;
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
+d3.csv('../data/populationOverTime.csv', d => {
   return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
+    year: new Date(+d.Year, 0, 1),
+    country: d.Entity,
+    population: +d.Population
   }
 }).then(data => {
   console.log('data :>> ', data);
 
   // SCALES
 
+  const xScale = d3.scaleTime()
+  .domain(d3.extent(data, d=> d.year))
+  .range([margin, wight-margin])
+
+  const yScale = d3.scaleLinear()
+  .domain(d3.extent(data, d=> d.population))
+  .range([height-margin, margin])
+
   // CREATE SVG ELEMENT
+
+  const svg = d3.select(#container)
+  .append("svg")
+  .attr("widht", width)
+  .attr("height", height)
 
   // BUILD AND CALL AXES
 
