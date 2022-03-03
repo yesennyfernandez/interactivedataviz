dc4ce13906b9b4c57d26b6f72c2b78a0b24f4a60
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-03-02 20:50:27 -0500

    changes

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -25,15 +25,27 @@ d3.csv('../data/populationOverTime.csv', d => {
 
   // CREATE SVG ELEMENT
 
-  const svg = d3.select(#container)
+  const svg = d3.select("#container")
   .append("svg")
   .attr("widht", width)
   .attr("height", height)
 
   // BUILD AND CALL AXES
 
+
   // LINE GENERATOR FUNCTION
 
+  const lineGen = d3.line()
+.x(d => xScale(d.year))
+.y(d => yScale(d.Population))
+
   // DRAW LINE
+svg.selectAll("path.line")
+.data([data])
+.join('path')
+.attr("class", "Line")
+.attr("stroke", "blue")
+.attr("fill", "none")
+.attr("d" d => lineGen(d))
 
 });
