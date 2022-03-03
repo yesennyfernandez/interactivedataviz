f3dbf87d809e45e3e9e166b7d3476cdddfdc5ce3
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-03-02 19:50:56 -0500

    Added Changes

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,29 @@
 console.log('hello world');
+    const label = document.getElementById("Label")
+    const input = document.getElementById("Input")
+    const button = document.getElementById("Submit")
+
+    let Name;
+
+    function NameUpdate(){
+    Name = "Mary"
+    console.log (Name)
+    label.innerText = "Wow! I love the name: "+
+    Name
+    button.innerText = "Name it again!"
+    }
+
+    // This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+    }
+    // This function display values
+    function display(value) {
+    document.getElementById("result").value += value;
+    }
+    // This function evaluates the expression and return result
+    function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+    }
--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,8 +1,6 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+height = window.innerHeight * 0.7;
 
 /* LOAD DATA */
 d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
@@ -10,7 +8,27 @@ d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
     console.log(data)
 
     /* SCALES */
+    cons xScale = d3.scaleLinear ()
+    .domain([0,d3.max(data, d => d.envScore2020)])
+    .range([0,width-margin])
+
+    const yScale = d3.scaleLinear()
+    .domain([0,d3.max(data, d => d.ideologyScore2020)])
+    .range(height-margin, 0)
     
     /* HTML ELEMENTS */
-    
+    const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+    /* JOIN - SELECT-DATA-JOIN & DRAW */
+    svg.selectAll("circle")
+    .data(data)
+    .join('circle')
+    .attr("cx", d => xScale(d.envScore2020))
+    .attr("cy", d => yScale(d.ideologyScore2020))
+    .attr("r", 3)
+
+
   });
