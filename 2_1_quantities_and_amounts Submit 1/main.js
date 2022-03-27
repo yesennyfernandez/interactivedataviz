de008a4fa5adea83cc67fcd14a3669b84849ca1a
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-03-27 16:46:10 -0400

    change

--- 2_1_quantities_and_amounts Submit 1/main.js
+++ 2_1_quantities_and_amounts Submit 1/main.js
@@ -3,60 +3,53 @@ const width = window.innerWidth *.8 ;
 const height = 500;
 
 /* LOAD DATA */
+var svg = d3.select("#container")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
 d3.csv('../data/squirrelActivities.csv', d3.autoType)
-.then(data => {
-  console.log("data", data)
+  .then(data => {
+    console.log("data", data)
 
-  /* SCALES */
-  // xscale - categorical, activity
- // const xScale = d3.scaleBand()
-    //.domain(data.map(d=> d.activity))
-   // .range([0, width]) // visual variable
-    //.paddingInner(.2)
-
+    /* SCALES */
+    // xscale - categorical, activity
     const xScale = d3.scaleLinear()
       .domain([0, d3.max(data, d=> d.count)])   //data.map(d=> d.activity))
-      .range([0, width]);
-
-      svg.append("g")
+      .range([0, width]); //visual variable
+      //.ticks(14);
+    
+    svg.append("g")
       .attr("transform", "translate(0, " + height + ")")
       .call(d3.axisBottom(xScale).ticks(14))
       .selectAll("text")
       .attr("transform", "translate(0,0)rotate(-45)")
       .style("text-anchor", "end");
 
-
-    // yscale - linear,count
+    //yscale - linear, count
     const yScale = d3.scaleBand()
-    .domain(data.map(d=> d.activity))
-    .range([0, height])
-    .paddingInner(0.1);
-    
-    svg.append("g")
-      .call(d3.axisLeft(yScale))  
-      
-
-  //  const yScale = d3.scaleLinear()
-    //.domain([0, d3.max(data, d=> d.count)])
-    //.range([height, 0])
-
-  /* HTML ELEMENTS */
-  // svg
-  const svg = d3.select("#container")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
+      .domain(data.map(d=> d.activity))
+      .range([0, height])
+      .paddingInner(0.1);
 
     svg.append("g")
       .call(d3.axisLeft(yScale))
+    
 
-  // bars
-  svg.selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("width", xScale.bandwidth())
-    .attr("height", d=> height - yScale(d.count))
-    .attr("x", d=>xScale(d.activity))
-    .attr("y", d=> yScale(d.count))
-    .attr("fill", "#000080")
-})
+    svg.selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", xScale(0))
+      .attr("y", function(d) { return yScale(d.activity); })
+      .attr("width", function(d) { return xScale(d.count); }) //)
+      .attr("height",  yScale.bandwidth() ) // d=> height - yScale(d.count))
+      .attr("fill", "#69b3a2")
+      //.attr("x", d=>xScale(d.activity))
+      //.attr("y", d=>yScale(d.count))
+    /** Select your container and append the visual elements to it */
+
+  })
