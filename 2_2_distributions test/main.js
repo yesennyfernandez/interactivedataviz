401228a1c8d444533c6c69a6c39ee29e5a300251
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-03-29 11:25:51 -0400

    changes

---
+++ 2_2_distributions test/main.js
@@ -0,0 +1,67 @@
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * 0.8,
+height = window.innerHeight * 0.8,
+margin = {top: 20, bottom: 35, left: 30, right: 10};
+
+
+/* LOAD DATA */
+d3.json("dv-complaint-radio-run-2021 (Int_Data_Viz).CSV", d3.autoType)
+.then(data => {
+ console.log(data)
+
+ /* SCALES */
+
+ const xScale = d3.scaleLinear()
+   .domain([0,d3.max(data, d => d.envScore2020)])
+   .range([margin.left, width-margin.right])
+
+ const yScale = d3.scaleLinear()
+   .domain([0,d3.max(data, d => d.ideologyDV)])
+   .range([height-margin.bottom,margin.top])
+
+ const colorScale = d3.scaleOrdinal()
+   .domain(["Rape Complaints", "Felony Assault Complaints", "Murder Complaints"])
+   .range(["red","blue","purple"])
+
+ /* ELEMENTS */
+
+ const svg = d3.select("#container")
+   .append("svg")
+   .attr("width", width)
+   .attr("height", height)
+
+ const xAxis = d3.axisBottom(xScale)
+ const yAxis = d3.axisLeft(yScale)
+
+ svg.append("g")
+   .attr("transform", `translate(0,${height-margin.bottom})`)
+   .call(xAxis)
+
+ svg.append("g")
+   .attr("transform", `translate(${margin.left},0)`)
+   .call(yAxis)
+
+ /* SELECTALL-DATA-JOIN-DRAW */
+ /* +ATTRIBUTES */
+
+    /* 5 - JOIN - SELECT-DATA-JOIN & DRAW */
+     /* 6 - ATTRIBUTES TO FINISH DRAWING GRAPHICS */
+
+    svg.selectAll("circle")
+      .data(data)
+      .join(
+        enter => enter
+          .append("circle")
+          .attr("r", 1)
+          .attr("cx", d => xScale(d.envDV))
+          .attr("cy", d => yScale(d.ideologyDV))
+          .attr("fill", "black")
+            .transition()
+            .duration(4000) // in ms
+            .delay(200)
+              .attr("r", 10)
+              .attr("fill", d=> colorScale(d.Party))
+      )
+
+    
+  });
