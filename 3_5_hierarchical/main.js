4edc294eecd8b4a24588e370acbce5b1e561c23b
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-04-06 17:03:41 -0400

    c

--- 3_5_hierarchical/main.js
+++ 3_5_hierarchical/main.js
@@ -1,14 +1,16 @@
 /**
  * CONSTANTS AND GLOBALS
  * */
-const width = window.innerWidth * 0.9,
-height = window.innerHeight * 0.7,
-margin = {top: 20, bottom: 35, left: 30, right: 10};
+ const width = window.innerWidth * 0.9,
+ height = window.innerHeight * 0.7,
+ margin = { top: 20, bottom: 50, left: 60, right: 40 };
+
+let svg;
+let tooltip;
 
 /**
 * APPLICATION STATE
 * */
-
 let state = {
   data: null,
   hover: null
@@ -19,6 +21,7 @@ let state = {
 * */
 d3.json("../data/flare.json", d3.autotype).then(data => {
   state.data = data;
+  console.log(state.data)
   init();
 });
 
@@ -29,73 +32,80 @@ d3.json("../data/flare.json", d3.autotype).then(data => {
 function init() {
 
 const container = d3.select("#container")
-.style("position","relative");
+  .style("position","relative");
 
-svg= container
-.append("svg")
-.attr("width", width)
-.attr("height", height)
+svg = container 
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 tooltip = container.append("div")
-.attr("class", "tooltip")
-.style("top",0)
-.style("left",0)
-.style("color", "white")
-.style("position", "absolute");
+  .attr("class", "tooltip")
+  .style("top",0)
+  .style("left",0)
+  .style("color", "white")
+  .style("position", "absolute");
 
 const root = d3.hierarchy(state.data)
-.sum(d => d.value)
-.sort((a,b) => b.value - a.value);
-console.log(root)
+  .sum(d => d.value)
+  .sort((a,b) => b.value - a.value);
+  console.log(root)
 
 const treeLayout = d3.treemap()
-.size([
+  .size([
   width - margin.left - margin.right,
   height - margin.top - margin.bottom])
-.paddingInner(2)
+  .paddingInner(2);
 
-const tree = treeLayout(root)
-const leaves = tree.leaves() //that "leaves" in tree.leaves is a function that actually goes there.
+  const tree = treeLayout(root)
+  const leaves = tree.leaves()
 
-const leafGroups = svg
-.selectAll("g")
-      .data(leaves)
-      .join("g")
-      .attr("transform", d => `translate(${d.x0}, ${d.y0})`)
+  console.log(tree)
+  console.log(leaves)
 
-      leafGroups.append("rect")
-      .attr("fill", "purple")
-      .attr("stroke", "gray")
-      .attr("width", d => d.x1 - d.x0)
-      .attr("height", d => d.y1 - d.y0)
+  const leafGroups = svg
+    .selectAll("g")
+    .data(leaves)
+    .join("g")
+    .attr("transform", d => `translate(${d.x0}, ${d.y0})`)
 
-      leafGroups
-      console.on("mouseenter", (event, d) => {
-state.hover = {
-  position: [d.x0, d.y0],
-  name: d.data.name
+  leafGroups.append("rect")
+    .attr("fill", "purple")
+    .attr("stroke", "gray")
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+
+  leafGroups
+    .on("mouseenter", (event, d) => {
+      state.hover = {
+        position: [d.x0, d.y0],
+        name: d.data.name
       }
-    draw()
-    console.log(state.hover)
-      })
-      console.on("mouseleave", () =>{
-  state.hover = null
-  draw();
-  console.log(state.hover)
-})
-draw(); // calls the draw function
+      draw()
+      console.log(state.hover)
+    })
+    .on("mouseleave", () => {
+      state.hover = null
+      draw();
+      console.log(state.hover)
+    })
+
+  draw(); // calls the draw function
 }
+
 /**
 * DRAW FUNCTION
 * we call this every time there is an update to the data/state
 * */
 function draw() {
-  if (state.hover){
+  if (state.hover) {
     tooltip
     .html(`<div>${state.hover.name}</div>`)
     .transition()
-    .duration(700)
-    style("opacity", 0.9)
-    .style("transform",`translate(${state.hover.position [0]}px,${state.hover.position[1]}px)`)
+    .duration(400)
+    .style("opacity", 0.9)
+    .style("transform",`translate(${state.hover.position[0]}px,${state.hover.position[1]}px)`)
+
   }
+
 }
